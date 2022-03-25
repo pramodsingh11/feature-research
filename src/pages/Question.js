306b22d6bdeb6c0bb90dab5dbb685acd8fba6a8d
Pramod singh <pramod11@Pramods-MacBook-Air.local>
2022-03-25 16:10:25 +0530

    build

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -1,4 +1,4 @@
-import { React, useEffect, useState } from "react";
+import { React, useState } from "react";
 import axios from "axios";
 import "../custom.scss";
 import ThankYou from "./ThankYou";
